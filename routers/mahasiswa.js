3a2d99350874149a060566986308d98ea3fb7903
ranipatricia <119095924+ranipatricia@users.noreply.github.com>
2024-07-16 05:37:15 +0700

    Add files via upload

---
+++ routers/mahasiswa.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const routerMahasiswa = express.Router()
+const controllerMahasiswa = require("../controllers/mahasiswa")
+
+routerMahasiswa.route('/mahasiswa')
+    .post(controllerMahasiswa.insert)
+    .get(controllerMahasiswa.getMahasiswa)
+
+routerMahasiswa.route('/mahasiswa/:nim')
+    .get(controllerMahasiswa.getMahasiswaByNim)
+    .put(controllerMahasiswa.update)
+    .delete(controllerMahasiswa.delete)
+
+module.exports = routerMahasiswa
